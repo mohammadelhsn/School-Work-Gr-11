db974d790b6706d7c05e99e654785df162605807
DeltaAbstract <51869374+DeltaAbstract@users.noreply.github.com>
2021-11-26 10:42:35 -0500

    e

--- New folder/something.py
+++ New folder/something.py
@@ -1,7 +1,16 @@
 import re 
 
-mo = "mo "
-a = mo.strip()
+# mo = "mo "
+# a = re.sub(r'\s+', '', mo)
+# print(a)
+# if (a == "mo"): print("worked")
+# else: print("not worked")
 
+mo = "mo s"
+b = (r'\s+', '', mo)
+print(str(b))
+a = mo.replace(r'\s+', '')
+
+print(a)
 if (a == "mo"): print("worked")
 else: print("not worked")
